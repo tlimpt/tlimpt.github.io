a30125a9a5efbd7078def451618f0795898dc8e9
tlimpt <43952302+tlimpt@users.noreply.github.com>
2020-12-22 18:23:23 +0000

    Delete escuro.js

--- escuro.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-	var b = document.createElement("a");
-	b.innerHTML = "★";
-	b.setAttribute("onclick", "document.body.classList.toggle('e');");
-	b.className = "flutua";
-	document.body.appendChild(b);
-	window.matchMedia('(prefers-color-scheme: dark)').matches && document.body.classList.toggle('e');
-}
