d9fa38da52069dd4e90f9ef71d8a1c56de3f5b25
tlimpt <43952302+tlimpt@users.noreply.github.com>
2021-11-04 22:50:47 +0000

    Update z-pp.js

--- z-pp.js
+++ z-pp.js
@@ -7,15 +7,14 @@ import { readJSON, writeJSON, removeFile } from 'https://deno.land/x/flat@0.0.11
 // Step 1: Read the downloaded_filename JSON
 const filename = Deno.args[0]; // Same name as downloaded_filename `const filename = 'btc-price.json';`
 const json = await readJSON(filename);
-const obj = JSON.parse(json);
 
 // Step 2: Filter specific data we want to keep and write to a new JSON file
-const p = Object.values(obj.chart.result[0].meta.regularMarketPrice);
-const t = Object.values(obj.chart.result[0].meta.regularMarketTime);
+const p = Object.values(json.chart.result[0].meta.regularMarketPrice);
+const t = Object.values(json.chart.result[0].meta.regularMarketTime);
 const texto = 'let p = '.concat(p, ', t = ', t, ';');
 
 // Step 3. Write a new JSON file with our filtered data
-const newFilename = `z.js`; // name of a new file to be saved
+const newFilename = 'z.js'; // name of a new file to be saved
 await writeJSON(newFilename, texto); // create a new JSON file with just the Bitcoin price
 
 // Optionally delete the original file
